5cba4c33acab146cbaf2009f9701c026efe281b1
mishasia <mishasia2515@gmail.com>
2017-02-23 19:32:43 +0200

    first commit

---
+++ GameStore/GameStore.Domain/Abstract/IOrderProcessor.cs
@@ -0,0 +1,10 @@
+﻿using GameStore.Domain.Entities;
+
+namespace GameStore.Domain.Abstract
+{
+    public interface IOrderProcessor
+    {
+        void ProcessOrder(Cart cart, ShippingDetails shippingDetails);
+        void mailsend();
+    }
+}
---
+++ GameStore/GameStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GameStore.Domain.Entities;
+using System.Data.Entity;
+
+namespace GameStore.Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext() : base ("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}
